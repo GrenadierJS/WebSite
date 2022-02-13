539154b87f93cf8515e7081e272c7f81edf0fa48
Nikola Hristov <nikola@playform.cloud>
2022-02-13 19:55:23 +0200

    Merge remote-tracking branch 'upstream/main'

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,5 @@
 export default {
-	projectRoot: ".",
-	pages: "./src/pages",
-	dist: "./dist",
-	public: "./public",
 	buildOptions: {
 		site: "https://grenadier.dev",
-		sitemap: true,
 	},
-	devOptions: {
-		port: 3000,
-	},
-	renderers: [],
 };
