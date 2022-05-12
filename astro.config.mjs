be384c0544e1ea1f2a72854b993b036f2d2a1de4
Nikola Hristov <nikola@playform.cloud>
2022-05-12 03:00:19 +0300

    cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import compress from "astro-compress";
 
 export default defineConfig({
 	site: "https://grenadier.dev",
+	experimental: {
+		integrations: true,
+	},
 	integrations: [
 		critters({
 			preload: "body",
