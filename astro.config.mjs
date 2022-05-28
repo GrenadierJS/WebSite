0e22f336480b1a6261bb74575c546969ee299799
Nikola Hristov <nikola@playform.cloud>
2022-05-28 16:32:14 +0300

    Merge and build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,7 @@ import critters from "astro-critters";
 import compress from "astro-compress";
 
 export default defineConfig({
-<<<<<<< HEAD
 	site: "https://grenadier.dev",
-=======
-	// TODO Place your site URL here
-	// site: "",
->>>>>>> upstream/main
 	experimental: {
 		integrations: true,
 	},
