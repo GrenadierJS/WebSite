f3325a53fe37a8ae4defc525f55aca645053cd55
Nikola Hristov <nikola@playform.cloud>
2022-08-28 00:17:21 +0300

    Cleanup

--- src/scripts/firebase.ts
+++ src/scripts/firebase.ts
@@ -1,8 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
 	apiKey: "AIzaSyA2-PGZUDCvuMP-et1ajAWb2r6QXY4WGl4",
 	appId: "1:16903102826:web:f85c7fd237548c3edf4513",
@@ -14,5 +11,4 @@ const firebaseConfig = {
 	storageBucket: "grenadierjs.appspot.com",
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
