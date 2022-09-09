21937e39965232d8b2ec9037070e032336fcbecd
Nikola Hristov <nikola@lightrix.dev>
2022-09-09 12:39:47 +0300

    Cleanup

--- astro.config.ts
+++ astro.config.ts
@@ -4,15 +4,13 @@ import critters from "astro-critters";
 
 import sitemap from "@astrojs/sitemap";
 
-export default (): AstroUserConfig => {
-	return {
-		site: "https://grenadier.dev",
-		integrations: [
-			sitemap(),
-			critters(),
-			compress({
-				logger: 1,
-			}),
-		],
-	};
-};
+export default (): AstroUserConfig => ({
+	site: "https://grenadier.dev",
+	integrations: [
+		sitemap(),
+		critters(),
+		compress({
+			logger: 1,
+		}),
+	],
+});
