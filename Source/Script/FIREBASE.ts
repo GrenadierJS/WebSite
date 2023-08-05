6e395cb0887a8d5530f41878da58a81611500835
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-06 01:44:44 +0300

    squash!

--- Source/Script/FIREBASE.ts
+++ Source/Script/FIREBASE.ts
@@ -1,6 +1,6 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp as Initialize } from "firebase/app";
 
-const firebaseConfig = {
+Initialize({
 	apiKey: "AIzaSyA2-PGZUDCvuMP-et1ajAWb2r6QXY4WGl4",
 	authDomain: "grenadierjs.firebaseapp.com",
 	databaseURL: "https://grenadierjs.firebaseio.com",
@@ -9,6 +9,4 @@ const firebaseConfig = {
 	messagingSenderId: "16903102826",
 	appId: "1:16903102826:web:f85c7fd237548c3edf4513",
 	measurementId: "G-EQHF084T2N",
-};
-
-initializeApp(firebaseConfig);
+});
