b2692ea9a0cc954d65a8f98e61115e0c0bfa9b9f
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-14 06:29:07 +0300

    squash!

--- Source/Script/Firebase.ts
+++ Source/Script/Firebase.ts
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 
 initializeApp({
-<<<<<<< HEAD
 	apiKey: "AIzaSyA2-PGZUDCvuMP-et1ajAWb2r6QXY4WGl4",
 	authDomain: "grenadierjs.firebaseapp.com",
 	databaseURL: "https://grenadierjs.firebaseio.com",
@@ -10,14 +9,4 @@ initializeApp({
 	messagingSenderId: "16903102826",
 	appId: "1:16903102826:web:f85c7fd237548c3edf4513",
 	measurementId: "G-EQHF084T2N",
-=======
-	apiKey: "", // TODO Replace with your API key
-	appId: "", // TODO Replace with your app ID
-	authDomain: "", // TODO Replace with your auth domain
-	databaseURL: "", // TODO Replace with your database URL
-	measurementId: "", // TODO Replace with your measurement ID
-	messagingSenderId: "", // TODO Replace with your messaging sender ID
-	projectId: "", // TODO Replace with your project ID
-	storageBucket: "", // TODO Replace with your storage bucket
->>>>>>> upstream/main
 });
