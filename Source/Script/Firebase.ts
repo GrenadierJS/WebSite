ecc8d54379829e605d6a4ab3617f67d664410a8f
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-15 07:08:35 +0300

    squash!

--- Source/Script/Firebase.ts
+++ Source/Script/Firebase.ts
@@ -1,5 +1,4 @@
 export default (await import("firebase/app")).initializeApp({
-<<<<<<< HEAD
 	apiKey: "AIzaSyA2-PGZUDCvuMP-et1ajAWb2r6QXY4WGl4",
 	authDomain: "grenadierjs.firebaseapp.com",
 	databaseURL: "https://grenadierjs.firebaseio.com",
@@ -8,14 +7,4 @@ export default (await import("firebase/app")).initializeApp({
 	messagingSenderId: "16903102826",
 	appId: "1:16903102826:web:f85c7fd237548c3edf4513",
 	measurementId: "G-EQHF084T2N",
-=======
-	apiKey: "", // TODO Replace with your API key
-	appId: "", // TODO Replace with your app ID
-	authDomain: "", // TODO Replace with your auth domain
-	databaseURL: "", // TODO Replace with your database URL
-	measurementId: "", // TODO Replace with your measurement ID
-	messagingSenderId: "", // TODO Replace with your messaging sender ID
-	projectId: "", // TODO Replace with your project ID
-	storageBucket: "", // TODO Replace with your storage bucket
->>>>>>> upstream/main
 });
